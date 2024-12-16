e80b0b61d504a16b875c2c3c23ce5c7194c2a0d8
yasingulec <yasin.gulec94@outlook.com>
2024-12-16 18:27:06 +0300

    rendering the ball and, ball movement

--- RayPong/Program.cs
+++ RayPong/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using Raylib_cs;
+using RayPong.src;
+
+int screenWidth = 1280;
+int screenHeight = 800;
+
+Raylib.InitWindow(screenWidth, screenHeight, "RayPong");
+Raylib.SetTargetFPS(60);
+
+// initializing the ball object
+var ball = new Ball
+{
+    color = Color.White,
+    radius=10f,
+    x=screenWidth/2,
+    y=screenHeight/2,
+    xSpeed=10,
+    ySpeed=10,
+};
+
+while (!Raylib.WindowShouldClose())
+{
+    
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.Black);
+    ball.Render();
+    ball.Update();
+
+    Raylib.EndDrawing();
+}
+
+Raylib.CloseWindow();
---
+++ RayPong/src/Ball.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+namespace RayPong.src
+{
+    public class Ball
+    {
+        public int x { get; set; }
+
+        public int y { get; set; }
+
+        public float radius { get; set; }
+
+        public Color color { get; set; }
+
+        public int xSpeed { get; set; }
+
+        public int ySpeed { get; set; }
+
+        public void Render()
+        {
+            Raylib.DrawCircle(x, y, radius, color);
+
+            if (y + radius >= Raylib.GetRenderHeight() ||y - radius <=0)
+            {
+                ySpeed *= -1;
+            }
+
+            if (x + radius >= Raylib.GetRenderWidth() || x - radius <= 0)
+            {
+                xSpeed *= -1;
+            }
+        }
+
+        public void Update()
+        {
+            x += xSpeed;
+            y += ySpeed;
+        }
+    }
+}
